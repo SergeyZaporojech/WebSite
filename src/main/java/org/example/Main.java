2b1c58cb866dad29eb070eb9e0e84234b4025ad1
SergeyZaporojech <91011052+SergeyZaporojech@users.noreply.github.com>
2023-09-03 12:21:54 +0300

    init

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+@SpringBootApplication
+
+public class Main {
+    public static void main(String[] args) {
+System.out.println("Привіт! Команда.");
+
+    SpringApplication.run(Main.class,args);
+
+    }
+}
