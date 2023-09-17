509ab8a6b505a6661fa02948d5bd9a08e7ca79e3
SergeyZaporojech <91011052+SergeyZaporojech@users.noreply.github.com>
2023-09-17 21:53:02 +0300

    fixed

--- src/main/java/org/example/controller/ProductController.java
+++ src/main/java/org/example/controller/ProductController.java
@@ -3,6 +3,8 @@ package org.example.controllers;
 import lombok.AllArgsConstructor;
 import org.example.dto.product.ProductCreateDTO;
 import org.example.dto.product.ProductItemDTO;
+import org.example.dto.product.ProductUpdateDTO;
+import org.example.entities.ProductEntity;
 import org.example.entities.ProductImageEntity;
 import org.example.mappers.ProductMapper;
 import org.example.repositories.ProductImageRepository;
@@ -16,6 +18,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @AllArgsConstructor
@@ -44,9 +47,54 @@ public class ProductController {
         }
         return ResponseEntity.ok().body(productMapper.productToItemDTO(p));
     }
+    @PutMapping(value = "{id}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<ProductEntity> updateProduct(@PathVariable int id, @ModelAttribute ProductUpdateDTO dto) {
+        Optional<ProductEntity> existingProduct = productRepository.findById(id);
+
+        if (existingProduct.isPresent()) {
+            ProductEntity product = productMapper.productByUpdateProductDTO(dto);
+            product.setImages(new ArrayList<>());
+            product.setId(id);
+
+            for (ProductImageEntity existingImage : existingProduct.get().getImages()) {
+                storageService.removeFile(existingImage.getImage());
+                productImageRepository.deleteById(existingImage.getId());
+            }
+
+            for (MultipartFile image : dto.getImages()) {
+                String fileName = storageService.saveImageFormatFile(image, FileSaveFormat.WEBP);
+                ProductImageEntity productImage = new ProductImageEntity();
+                productImage.setImage(fileName);
+                productImage.setProduct(product);
+                product.getImages().add(productImage);
+            }
+
+            productRepository.save(product);
+            return ResponseEntity.ok().body(product);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
     @GetMapping()
     public ResponseEntity<List<ProductItemDTO>> getAllCategories() {
         return ResponseEntity.ok(productMapper.listProductsToItemDTO(productRepository.findAll()));
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable int id) {
+        Optional<ProductEntity> optionalProduct = productRepository.findById(id);
+        if (optionalProduct.isPresent()) {
+            ProductEntity product = optionalProduct.get();
+            List<ProductImageEntity> productImages = product.getImages();
+            for (ProductImageEntity productImage : productImages) {
+                storageService.removeFile(productImage.getImage());
+            }
+            productRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 }
